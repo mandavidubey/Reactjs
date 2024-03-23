0e7daaa2f3e6f770d2e2cb1b819411be49a45c2e
Mandavi Dubey <mandavidubey2002@gmail.com>
2024-03-23 14:48:40 +0530

    To Do

---
+++ 10toDo/src/components/TodoForm.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { useTodo } from "../contexts/TodoContext";
+
+function TodoForm() {
+  const [todo, setTodo] = useState("");
+  const { addTodo } = useTodo();
+
+  const add = (e) => {
+    e.preventDefault();
+
+    if (!todo) return;
+
+    addTodo({ todo, completed: false });
+    setTodo("");
+  };
+
+  return (
+    <form onSubmit={add} className="flex">
+      <input
+        type="text"
+        placeholder="Write Todo..."
+        className="w-full border border-gray-400 rounded-md py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button
+        type="submit"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Add
+      </button>
+    </form>
+  );
+}
+
+export default TodoForm;
